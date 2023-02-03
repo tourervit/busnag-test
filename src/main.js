eaab86b3fce6819956428ec16a628c9a14dc8f6e
Vitaly <tourervit@gmail.com>
2023-02-03 20:32:35 +0800

    sdf

--- src/main.js
+++ src/main.js
@@ -7,13 +7,14 @@ import BugsnagPluginVue from "@bugsnag/plugin-vue";
 
 
 Bugsnag.start({
-  appVersion: "3.0.1",
+  appVersion: "3.0.2",
   apiKey: "ceb06d2f1086242c28693908fe8f0241",
   plugins: [new BugsnagPluginVue()],
+  releaseStage: ['production']
 });
 const bugsnagVue = Bugsnag.getPlugin("vue");
 
 createApp(App).use(store).use(router).use(bugsnagVue).mount("#app");
 
-Bugsnag.notify(new Error("error introduced in 3.0.1"));
+Bugsnag.notify(new Error("error introduced in 3.0.2"));
 console.log(process.env.NODE_ENV)
